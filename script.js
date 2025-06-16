4b54376555fecf87a37342ce5043d6e749a1cb40
AnoSan <anomitra.santra.mat24@itbhu.ac.in>
2025-06-16 12:46:57 +0530

    Update the interface and add logic

--- script.js
+++ script.js
@@ -4,3 +4,91 @@ let result=document.querySelector(".result");
 buttons.forEach(btn=>{
     btn.addEventListener("click",e=>result.textContent+=btn.textContent);
 })
+let op='';
+let num1='';
+let num2='';
+const per=document.querySelector("#per");
+const equals=document.querySelector("#equals");
+const add=document.querySelector("#add");
+const minus=document.querySelector("#minus");
+const multiply=document.querySelector("#multiply");
+const divide=document.querySelector("#divide");
+const expo=document.querySelector("#expo");
+const clear=document.querySelector("#clear");
+allClear=document.querySelector("#allClear");
+add.addEventListener("click",e=>{
+    if(result.textContent!=''){
+        num1=result.textContent;
+        op='+';
+        result.textContent='';
+    }
+})
+allClear.addEventListener("click",e=>{
+    result.textContent='';
+    num1='';
+    num2='';
+    op='';
+})
+per.addEventListener("click",e=>{
+    if(result.textContent!=''){
+        result.textContent=Number(result.textContent)/100;
+    }
+})
+minus.addEventListener("click",e=>{
+    if(result.textContent!=''){
+        num1=result.textContent;
+        op='-';
+        result.textContent='';
+    }
+})
+multiply.addEventListener("click",e=>{
+    if(result.textContent!=''){
+        num1=result.textContent;
+        op='*';
+        result.textContent='';
+    }
+})
+divide.addEventListener("click",e=>{
+    if(result.textContent!=''){
+        num1=result.textContent;
+        op='/';
+        result.textContent='';
+    }
+})
+expo.addEventListener("click",e=>{
+    if(result.textContent!=''){
+        num1=result.textContent;
+        op='^';
+        result.textContent='';
+    }
+})
+equals.addEventListener("click",e=>{
+    if(result.textContent!='' && num1!=''){
+        num2=result.textContent;
+        switch(op){
+            case '+':
+                result.textContent=Number(num1)+Number(num2);
+                break;
+            case '-':
+                result.textContent=Number(num1)-Number(num2);
+                break;
+            case '*':
+                result.textContent=Number(num1)*Number(num2);
+                break;
+            case '/':
+                if(Number(num2)===0){
+                    result.textContent='Error';
+                }
+                else {
+                    result.textContent=Number(num1)/Number(num2);
+                }
+                break;
+            case '^':
+                result.textContent=Math.pow(Number(num1), Number(num2));
+                break;
+        }
+        num1='';
+        num2='';
+        op='';
+    }
+})
